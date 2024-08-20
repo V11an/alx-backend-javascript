618a3674c01659caf0738bf29f6e57c41249fdc4
V11an <allankertich@gmail.com>
2024-08-20 22:40:55 +0300

    a small HTTP server using Express module

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,15 +1,13 @@
 const express = require('express');
+
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('*', (req, res) => {
-  res.status(404).send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET /' + req.url + '</pre></body></html>');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
 });
+
+module.exports = app;
